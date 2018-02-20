92561cd41207de86e8bc12afde4b57c4976dda08
nabiljdaoud@gmail.com <nabiljdaoud@gmail.com>
2018-02-20 03:10:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()){
+  if (string === string.toLowerCase()) {
     return "I can't hear you!";
+  } elif (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } elif (string === "I love you, Grandma.") {
+    return "I love you, too.";
   }
+  
   return
 }
